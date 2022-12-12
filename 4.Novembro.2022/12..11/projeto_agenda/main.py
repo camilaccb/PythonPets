b651fa8609da806502d1c799d93a6da712a5dbc2
Camila Caldas <camisccb@gmail.com>
2022-12-11 22:06:41 -0300

    adicionando armazenamento da agenda de contatos em arquivo json

--- 4.Novembro.2022/12..11/projeto_agenda/main.py
+++ 4.Novembro.2022/12..11/projeto_agenda/main.py
@@ -12,62 +12,61 @@
 
 from menu import solicita_opcao_do_usuario
 from opcoes import ler_contato, adicionar_contato_na_agenda,mostrar_todos_os_contatos, recebe_nome_do_contato, procurar_o_nome_do_contato, excluir_contato, executar_operacao
+from json import loads
 
-
-base_de_contatos = {}
+json_output_file = "agenda.json"
+with open(json_output_file, mode="r", encoding="utf-8") as f:
+    base_de_contatos = loads(f.read())
 
 print("====== Bem vindo à sua agenda de contatos! ======")
 opcao_escolhida = solicita_opcao_do_usuario()
 
-# TODO: utilizar a opção do match
 
 while opcao_escolhida != 6:
 
-    if opcao_escolhida == 1:
+    match opcao_escolhida:
 
-        contato = ler_contato()
-        adicionar_contato_na_agenda(contato, base_de_contatos)
-        print("====== Escolha outra opção ======")
-        opcao_escolhida = solicita_opcao_do_usuario()
+        case 1:
+            contato = ler_contato()
+            adicionar_contato_na_agenda(contato, base_de_contatos)
+            print("====== Escolha outra opção ======")
+            opcao_escolhida = solicita_opcao_do_usuario()
 
-    elif opcao_escolhida == 2:
+        case 2:
+            nome_contato = recebe_nome_do_contato()
+            contato_encontrado = procurar_o_nome_do_contato(nome_contato,base_de_contatos)
 
-        nome_contato = recebe_nome_do_contato()
-        contato_encontrado = procurar_o_nome_do_contato(nome_contato,base_de_contatos)
-
-        if contato_encontrado == nome_contato:
-            alteracao = input('Qual alteração você deseja fazer? (1-nome do contato, 2-telefone do contato): ')
+            if  contato_encontrado != None:
+                alteracao = input('Qual alteração você deseja fazer? (1-nome do contato, 2-telefone do contato): ')
     
-        if alteracao == '1':
-            novo_nome_contato = input('Qual o novo nome do contato? ')
-            contato_alterado =[novo_nome_contato,base_de_contatos[nome_contato]]
-            excluir_contato(nome_contato,base_de_contatos)
-            adicionar_contato_na_agenda(contato_alterado,base_de_contatos)
+                if alteracao == '1':
+                    novo_nome_contato = input('Qual o novo nome do contato? ')
+                    contato_alterado =[novo_nome_contato,base_de_contatos[nome_contato]]
+                    excluir_contato(nome_contato,base_de_contatos)
+                    adicionar_contato_na_agenda(contato_alterado,base_de_contatos)
             
-        elif alteracao == '2':
-            novo_numero_contato = int(input('Qual o novo número do contato? '))
-            contato_alterado = [nome_contato, novo_numero_contato]
-            excluir_contato(nome_contato, base_de_contatos)
-            adicionar_contato_na_agenda(contato_alterado,base_de_contatos)
-            
+                elif alteracao == '2':
+                    novo_numero_contato = int(input('Qual o novo número do contato? '))
+                    contato_alterado = [nome_contato, novo_numero_contato]
+                    excluir_contato(nome_contato, base_de_contatos)
+                    adicionar_contato_na_agenda(contato_alterado,base_de_contatos)    
         
-        opcao_escolhida = solicita_opcao_do_usuario()
+            opcao_escolhida = solicita_opcao_do_usuario()
 
-    elif opcao_escolhida == 3:
-
-        executar_operacao(excluir_contato, base_de_contatos=base_de_contatos)
-        print("====== Escolha outra opção ======")
-        opcao_escolhida = solicita_opcao_do_usuario()
+        case 3:
+            executar_operacao(excluir_contato, base_de_contatos=base_de_contatos)
+            print("====== Escolha outra opção ======")
+            opcao_escolhida = solicita_opcao_do_usuario()
             
-    elif opcao_escolhida == 4:
-        executar_operacao(procurar_o_nome_do_contato,base_de_contatos=base_de_contatos)
-        print("====== Escolha outra opção ======")
-        opcao_escolhida = solicita_opcao_do_usuario()        
+        case 4:
+            executar_operacao(procurar_o_nome_do_contato,base_de_contatos=base_de_contatos)
+            print("====== Escolha outra opção ======")
+            opcao_escolhida = solicita_opcao_do_usuario()        
 
-    elif opcao_escolhida == 5:
-       mostrar_todos_os_contatos(base_de_contatos)
-       print("====== Escolha outra opção ======")
-       opcao_escolhida = solicita_opcao_do_usuario() 
+        case 5:
+            mostrar_todos_os_contatos(base_de_contatos)
+            print("====== Escolha outra opção ======")
+            opcao_escolhida = solicita_opcao_do_usuario() 
 
 
 print('A agenda está sendo encerrada')
--- 4.Novembro.2022/12..11/projeto_agenda/opcoes.py
+++ 4.Novembro.2022/12..11/projeto_agenda/opcoes.py
@@ -1,4 +1,5 @@
 from menu import solicita_opcao_do_usuario
+from json import dumps, loads
 
 
 def ler_contato() -> list:
@@ -24,16 +25,25 @@ def ler_contato() -> list:
     return contato
 
 
+def atualizar_arquivo_json(base_de_contatos: dict):
+    json_output_file = "agenda.json"
+    json_string = dumps(base_de_contatos, indent=1)
+    with open(json_output_file, mode = "w", encoding= "utf-8") as f:
+        f.write(json_string)
+        
+
 def adicionar_contato_na_agenda(contato: list, base_de_contatos: dict):
     
     nome = contato[0]
     telefone = contato[1] 
     base_de_contatos[nome] = telefone
+    atualizar_arquivo_json(base_de_contatos)
     
     # TODO: dar uma mensagem de erro quando tiver um contato repetido
     
     
     print("Contato adicionado com sucesso.")
+
     
 
 def mostrar_todos_os_contatos(base_de_contatos: dict):
@@ -52,19 +62,25 @@ def recebe_nome_do_contato():
 
 
 def procurar_o_nome_do_contato(nome_contato_procurado: str, base_de_contatos: dict):
-    
+
     mensagem_contato_não_encontrado = 'O contato não foi encontrado na agenda, tente novamente'
-    numero_contato = base_de_contatos.get(nome_contato_procurado,mensagem_contato_não_encontrado)
+    numero_contato = base_de_contatos.get(nome_contato_procurado)
     
-    print(f'{nome_contato_procurado}: {numero_contato}')
+    if numero_contato == None:
+        print(mensagem_contato_não_encontrado)
+        nome_contato_procurado = None
+    else:
+        print(f'{nome_contato_procurado}: {numero_contato}')
     
     return nome_contato_procurado
+        
 
 
 def excluir_contato(nome_contato_para_excluir: str, base_de_contatos: dict):
 
     mensagem_contato_não_encontrado = 'O contato não foi encontrado na agenda, tente novamente'
     base_de_contatos.pop(nome_contato_para_excluir,mensagem_contato_não_encontrado)
+    atualizar_arquivo_json(base_de_contatos)
 
 
 def executar_operacao(operacao, base_de_contatos: dict):
